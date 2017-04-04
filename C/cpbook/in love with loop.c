0ec6d5251833d8c49c51ef88780f6e9fd229bf76
nahinbinkaysar <nahin.binkaysar@gmail.com>
2017-04-04 22:11:07 +0600

    Update in love with loop.c

--- C/cpbook/in love with loop.c
+++ C/cpbook/in love with loop.c
@@ -1,57 +1,28 @@
+//problem-30 in love with loop
+//nahin bin kaysar
+//http://cpbook.subeen.com/2013/01/in-love-with-loop.html
+
 #include <stdio.h>
-
-int maxNum(int ara[])
-{
-    int i, max;
-    max = ara[0];
-    for(i=0;i<3;i++) {
-        if(ara[i]>max) max = ara[i];
-    }
-
-    return max;
-}
-
-int minNum(int ara[])
-{
-    int i, min;
-    min = ara[0];
-    for(i=0;i<3;i++) {
-        if(ara[i]<min) min = ara[i];
-    }
-
-    return min;
-}
-
-int midNum(int ara[])
-{
-    int i, max, min, mid;
-
-    min = minNum(ara);
-    max = maxNum(ara);
-    for(i=0;i<3;i++){
-        if(ara[i] != max && ara[i] != min) {
-            mid = ara[i];
-        }
-    }
-
-    return mid;
-}
 
 int main()
 {
-    int ara[3];
-    int A, B, C;
-    int t, T;
-    int a, i, j, k;
+    int a, b, c, x, y, z;
+    int t, T, temp, swap, i, j, ara[3];
+
     scanf("%d", &T);
+
     for(t=0;t<T;t++){
-        for(a=0;a<3;a++) {
-            scanf("%d", &ara[a]);
+        scanf("%d %d %d", &a, &b, &c);
+        printf("Case %d:\n",t+1);
+        for(x=0;x<=a;x++){
+            for(y=0;y<=b;y++){
+                for(z=0;z<=c;z++){
+                    if((x!=y && y!=z && x!=z)&&(x<=y && y<=z)){
+                        printf("%d %d %d\n",x,y,z);
+                    }
+                }
+            }
         }
-        A = minNum(ara);
-        B = midNum(ara);
-        C = maxNum(ara);
-        printf("%d %d %d\n",A,B,C);
     }
 
     return 0;

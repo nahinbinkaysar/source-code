17ca91d19769d1960830ab752011fab9c9f4259b
root <root@localhost>
2018-12-03 00:48:13 +0600

    i dont know what will happen

---
+++ date_comparer.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+typedef struct date{
+	int day, month, year;
+}
+int main()
+{
+	date one;
+	//these will be used to store data
+	scanf("%d %d %s", &date.day, &date.year, date.month);
+	
